3c7a6fdb5c8b6d53aaef6a7f666fcbf9660ff1a3
AFReynoso <areynoso@gmail.com>
2014-02-05 00:49:43 -0300

    Mas Rapido

--- zilean/Fase1.py
+++ zilean/Fase1.py
@@ -8,16 +8,8 @@ h,w = img.shape[:2]
 
 #img2 = np.zeros((h,w,1), np.uint8)
 
-'''
-for y in range(h):
-    for x in range(w):
-        r = img[y,x][2]
-        g = img[y,x][1]
-        img2[y,x] =  (r >= g) * 255
-'''
 r=img[:,:,2]
 g=img[:,:,1]
-#img2 = (r >= g) * 255
 img2 = (r>g).astype(np.uint8)*255
 
 cv2.imshow('Filtrada', img2)
